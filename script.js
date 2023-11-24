9368bac680e59ba5c36b43abeaffc39725b1b1bf
aivarovsky <ivarovskiy@yandex.ru>
2023-11-24 15:07:39 +0300

    refactor: extract script and adjust its code structure

---
+++ script.js
@@ -0,0 +1,38 @@
+const BREAKPOINT = 1024;
+
+const nav = document.getElementById("nav");
+const navToggle = document.getElementById("nav-toggle");
+
+visualViewport.addEventListener("resize", () => {
+    if (getWidth() < BREAKPOINT){
+        nav.style.display = "block";
+        if (!navToggle.checked){
+            nav.style.display = "none";
+        }
+
+    }
+    else {
+        nav.style.display = "flex";
+        navToggle.checked = false;
+    }
+  });
+
+function toggleNav() {
+    if (getWidth() < BREAKPOINT){
+        nav.style.display = "block";
+        if (navToggle.checked){
+            nav.style.display = "none";
+        }
+    }
+}
+
+function closeNav() {
+    if (getWidth() < BREAKPOINT){
+        nav.style.display = "none";
+        navToggle.checked = false;
+    }
+}
+
+function getWidth() {
+    return Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+}
